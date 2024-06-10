a6ee68f893bcaa476ed48ff8107920010a941e37
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-06-11 00:12:37 +0530

    final code changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
       <h1 className="text-4xl md:text-5xl font-bold mb-5">
         Welcome to my blog
       </h1>
-      <p className="max-w-[750px] mx-auto leading-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, a
-        natus? Dolores veritatis perferendis non doloribus numquam praesentium
-        aliquid tempora qui similique, exercitationem distinctio labore culpa
-        nam natus consequuntur rem!
-      </p>
+      <p className="max-w-[750px] mx-auto leading-8">Krishna Next js Blogs</p>
     </main>
   );
 }
